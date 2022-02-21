250b45f65c54cbe8b64845f4ffa28f3ffa98fc9b
F1cky <97504239+F1cky@users.noreply.github.com>
2022-02-21 18:08:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 config = {
-	"admin_id":1085176916,
-	'support':'@username',
-	"token":"",
+	"admin_id":'2070045449',
+	'support':'@FickyXXL',
+	"token":"5291783745:AAHIXhUJqGBAbh_fwmibhlbBgGCSU0slX5Y",
 	"percent":5,
-	"qiwi_phone":'',
-	"qiwi_token":'',
+	"qiwi_phone":'+380938430077',
+	"qiwi_token":'e65141e3f1ed808422eab67158bc152a',
 	"mysql":{
 		"login":"root",
 		"password":'root',
