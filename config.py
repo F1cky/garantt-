5b5ac810ec1a46bd59792dc9a69f0860e72fe49c
F1cky <97504239+F1cky@users.noreply.github.com>
2022-02-21 18:05:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+config = {
+	"admin_id":1085176916,
+	'support':'@username',
+	"token":"",
+	"percent":5,
+	"qiwi_phone":'',
+	"qiwi_token":'',
+	"mysql":{
+		"login":"root",
+		"password":'root',
+		"host":"localhost",
+		"db_name":"garant_bot",
+	}
+}
